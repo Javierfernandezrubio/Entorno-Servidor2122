4c52466431204df9b37841d7e4f7b558c953840d
Javier Fernandez Rubio <a19feruja@iesgrancapitan.org>
2022-02-24 10:22:01 +0100

    feat: añade index.php basico

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Index page
+ * 
+ * 
+ * @author Javier Fernández Rubio 
+ */
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="Javier Fernández Rubio">
+<title>Servidor de Javier</title>
+
+<!-- CSS -->
+<link href="css/styles.css" rel="stylesheet">
+
+<!-- JavaScript -->
+<script src=""></script>
+</head>
+<body>
+<noscript>
+<p>Bienvenido a Mi Sitio</p>
+<p>La página que estás viendo requiere para su funcionamiento el uso de JavaScript.Si lo has deshabilitado intencionadamente, por favor vuelve a activarlo.</p>
+</noscript>
+
+</body>
+</html>
